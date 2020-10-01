1cf297d95342a620c7474d2760281ecf03d677ce
Manumerous <manuel.galliker@gmx.ch>
2020-10-01 23:25:03 +0200

    working c++ application in docker container

--- README.md
+++ README.md
@@ -21,6 +21,9 @@ Execute the following command in the build folder
 You may then run the programm by using:  
 `./runApplication`
 
+provisional command:
+`docker run --device=/dev/video0:/dev/video0 -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -p 5000:5000 -p 8888:8888 -it --name opencvtest3 vafmr_ex1 /bin/bash`
+
 ### Links:
 
 https://www.sfml-dev.org/tutorials/2.5/start-linux.php
--- ex1/src/main.cpp
+++ ex1/src/main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <eigen3/Eigen/Dense>
+#include <opencv2/opencv.hpp>
 #include <iostream>
 #include <math.h>
 #include <time.h>
@@ -10,9 +11,30 @@
 #include <unistd.h>
 #endif
 
-int main()
-{
-    std::cout << "main function\n";
+// int main()
+// {
+//     std::cout << "main function\n";
 
+//     return 0;
+// }
+
+using namespace cv;
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        printf("usage: DisplayImage.out <Image_Path>\n");
+        return -1;
+    }
+    Mat image;
+    image = imread(argv[1], 1);
+    if (!image.data)
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    namedWindow("Display Image", WINDOW_AUTOSIZE);
+    imshow("Display Image", image);
+    waitKey(0);
     return 0;
 }
